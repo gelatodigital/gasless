1e73940d2d554f19e547b96d1621f89d2055358c
Ben Price <ben.jules.price@gmail.com>
2025-12-05 01:58:09 +0000

    feat: relayer sponsored example

---
+++ examples/relayer/sponsored/src/index.ts
@@ -0,0 +1,49 @@
+import {
+  createGelatoEvmRelayerClient,
+  type Status,
+  StatusCode,
+  sponsored
+} from '@gelatonetwork/ferry-sdk';
+import 'dotenv/config';
+import { baseSepolia } from 'viem/chains';
+
+const GELATO_API_KEY = process.env['GELATO_API_KEY'];
+
+if (!GELATO_API_KEY) {
+  throw new Error('GELATO_API_KEY is not set');
+}
+
+const main = async () => {
+  const relayer = createGelatoEvmRelayerClient({ apiKey: GELATO_API_KEY, testnet: true });
+
+  const hash = await relayer.sendTransaction({
+    chainId: baseSepolia.id,
+    data: '0xd09de08a',
+    payment: sponsored(),
+    to: '0xE27C1359cf02B49acC6474311Bd79d1f10b1f8De'
+  });
+
+  console.log(`hash: ${hash}`);
+
+  let status: Status;
+  while (true) {
+    status = await relayer.getStatus({ id: hash });
+
+    if (status.status !== StatusCode.Pending && status.status !== StatusCode.Submitted) {
+      break;
+    }
+  }
+
+  if (status.status === StatusCode.Confirmed) {
+    console.log(`transaction hash ${status.receipt.transactionHash}`);
+    process.exit(0);
+  } else {
+    console.log(`transaction failed, message: ${status.message}, data: ${status.data}`);
+    process.exit(1);
+  }
+};
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
