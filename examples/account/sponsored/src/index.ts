8074a70c4d87afcae920c6315ad783bef148fb7c
Ben Price <ben.jules.price@gmail.com>
2025-12-09 23:59:01 +0000

    feat: account sponsored example

---
+++ examples/account/sponsored/src/index.ts
@@ -0,0 +1,67 @@
+import {
+  createGelatoSmartAccountClient,
+  StatusCode,
+  sponsored,
+  toGelatoSmartAccount
+} from '@gelatonetwork/ferry-sdk';
+import 'dotenv/config';
+import { createPublicClient, type Hex, http } from 'viem';
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts';
+import { baseSepolia } from 'viem/chains';
+
+const GELATO_API_KEY = process.env['GELATO_API_KEY'];
+const PRIVATE_KEY = process.env['PRIVATE_KEY'];
+
+if (!GELATO_API_KEY) {
+  throw new Error('GELATO_API_KEY is not set');
+}
+
+const chain = baseSepolia;
+
+const main = async () => {
+  const owner = privateKeyToAccount((PRIVATE_KEY ?? generatePrivateKey()) as Hex);
+
+  const client = createPublicClient({
+    chain,
+    transport: http()
+  });
+
+  const account = toGelatoSmartAccount({
+    client,
+    owner
+  });
+
+  const relayer = await createGelatoSmartAccountClient({
+    account,
+    apiKey: GELATO_API_KEY,
+    testnet: chain.testnet
+  });
+
+  const hash = await relayer.sendTransaction({
+    calls: [
+      {
+        data: '0xd09de08a',
+        to: '0xE27C1359cf02B49acC6474311Bd79d1f10b1f8De'
+      }
+    ],
+    chainId: chain.id,
+    payment: sponsored()
+  });
+
+  console.log(`hash: ${hash}`);
+
+  const status = await relayer.waitForStatus({ id: hash });
+
+  if (status.status === StatusCode.Confirmed) {
+    console.log(`transaction hash ${status.receipt.transactionHash}`);
+    process.exit(0);
+  } else {
+    console.log(`transaction failed, message: ${status.message}, data: ${status.data}`);
+    process.exit(1);
+  }
+};
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
