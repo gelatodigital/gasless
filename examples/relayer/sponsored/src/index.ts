ece421cb8f6e5a4298d7fd843ae48723753d45f7
Ben Price <ben.jules.price@gmail.com>
2025-12-09 23:58:24 +0000

    refactor: relayer example declare chain

--- examples/relayer/sponsored/src/index.ts
+++ examples/relayer/sponsored/src/index.ts
@@ -8,11 +8,13 @@ if (!GELATO_API_KEY) {
   throw new Error('GELATO_API_KEY is not set');
 }
 
+const chain = baseSepolia;
+
 const main = async () => {
-  const relayer = createGelatoEvmRelayerClient({ apiKey: GELATO_API_KEY, testnet: true });
+  const relayer = createGelatoEvmRelayerClient({ apiKey: GELATO_API_KEY, testnet: chain.testnet });
 
   const hash = await relayer.sendTransaction({
-    chainId: baseSepolia.id,
+    chainId: chain.id,
     data: '0xd09de08a',
     payment: sponsored(),
     to: '0xE27C1359cf02B49acC6474311Bd79d1f10b1f8De'
