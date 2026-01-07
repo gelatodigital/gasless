71922712303eb3461d8f84991969c5d44c3c83de
Denis Panov <denis.panov@gmx.de>
2026-01-07 12:09:58 +0100

    feat: add new gelato_getStatus endpoint (#9)
    * feat: add Gelato status handling to EVM relayer
    
    - Introduced `getGelatoStatus` and `waitForGelatoStatus` actions for querying and waiting on Gelato transaction statuses.
    - Updated `GelatoEvmRelayerClient` to include new methods for Gelato status retrieval.
    - Added schemas for Gelato status and terminal status using Zod for validation.
    
    * feat: add gelato_getStatus endpoint
    
    - Introduced a new endpoint for retrieving Gelato transaction statuses.
    - Updated the changeset to reflect the patch for "@gelatocloud/gasless".

---
+++ src/relayer/evm/actions/waitForGelatoStatus.ts
@@ -0,0 +1,28 @@
+import type { Transport } from 'viem';
+import {
+  GelatoStatusCode,
+  type GelatoTerminalStatus,
+  type GetGelatoStatusParameters,
+  getGelatoStatus
+} from './getGelatoStatus.js';
+
+// TODO: use websockets
+// TODO: make polling interval configurable
+export const waitForGelatoStatus = async (
+  client: ReturnType<Transport>,
+  parameters: GetGelatoStatusParameters
+): Promise<GelatoTerminalStatus> => {
+  while (true) {
+    const status = await getGelatoStatus(client, parameters);
+
+    if (
+      status.status !== GelatoStatusCode.Pending &&
+      status.status !== GelatoStatusCode.Submitted &&
+      status.status !== GelatoStatusCode.Included
+    ) {
+      return status;
+    }
+
+    await new Promise((r) => setTimeout(r, 100));
+  }
+};
